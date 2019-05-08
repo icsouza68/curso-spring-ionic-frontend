a29fd3b940e0b11df1e8fe87c00bdb6997ce9c12
Ivan Souza <icsouza@yahoo.com>
2019-05-08 08:49:08 -0300

    Listando categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoriaService } from '../../services/domain/categoria.service';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -15,11 +16,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public categoriaService: CategoriaService) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
+    /** 
+     * como o http é assíncrono, o método subscribe é necessário para que a aplicação aguarde o 
+     * resposta do que foi chamado
+     * 
+     * response => {console.log(response)} é uma função anônima, onde response é o tipo do retorno
+     * e console.log é a função a ser executada. Ela será executada em caso de sucesso da resposta
+     * 
+     * error => será executado em caso de resposta com erro
+     * 
+     */
+    this.categoriaService.findAll().subscribe(response => {
+      console.log(response);
+    },
+    error => {
+      console.log(error);
+    });
+    
   }
 
 }
