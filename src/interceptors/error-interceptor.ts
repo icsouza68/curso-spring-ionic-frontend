27674586e186ad6fc788a3869a75469fdd658227
Ivan Souza <icsouza@yahoo.com>
2019-05-09 12:38:21 -0300

    Tratamento padrao para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
+import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor(public storage: StorageService, public alertCtrl: AlertController) {
+    }
 
     intercept( req: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
 //        console.log("passou");
@@ -11,19 +16,67 @@ export class ErrorInterceptor implements HttpInterceptor {
         .catch((error, caught) => {
 
             let errorObj = error;
+            
             if (errorObj.error) {
                 errorObj = errorObj.error;
-            }
+            };
 
-            if (!error.status) { // não está no formato JSON
+            if (!errorObj.status) { // não está no formato JSON
                 errorObj = JSON.parse(errorObj);
-            }
+            };
 
-            console.log("Erro detectado pelo interceptor");
-            console.log(errorObj);
+//            console.log("Erro detectado pelo interceptor");
+//            console.log(errorObj);
+//            console.log(errorObj.status);
+
+            switch(errorObj.status) {
+                case 401:
+//                    console.log("passou 401");
+                    this.handle401();
+                    break;
+
+                case 403: 
+                    this.handle403();
+                    break;
+
+                default:
+                    this.handleDefaultError(errorObj);
+            }
             
             return Observable.throw(errorObj);
         }) as any;
+    }
+
+    handleDefaultError(errorObj: any) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handle401(){
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handle403() {
+        this.storage.setLocalUser(null);
     }
 }
 
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -25,16 +25,23 @@ export class ProfilePage {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
-      .subscribe(response => {
-        this.cliente = response;
-        this.getImageIfExists();
-      },
-      error => {});
+        .subscribe(response => {
+          this.cliente = response;
+          this.getImageIfExists();
+        },
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
     }
+    else {
+      this.navCtrl.setRoot('HomePage');
+    };
   }
 
   getImageIfExists(){
-    console.log(`${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`);
+//    console.log(`${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`);
     this.clienteService.getImageFromBucket(this.cliente.id)
     .subscribe(response => {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
